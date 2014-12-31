65797a88ce4a94f5407be6db77d19cc008253447
kalel120 <imran_aust_cse@hotmail.com>
2014-12-31 15:42:14 +0600

    Project Main Backup_v1.0
    This is the Master Backup Copy of the project. Unchanged Since 23 June,
    2014. Contains simple commands and hardware control(light, fan on-off)

---
+++ ProjectJarvis/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ProjectJarvis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.lstCommands = new System.Windows.Forms.ListBox();
+            this.commandsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstCommands
+            // 
+            this.lstCommands.FormattingEnabled = true;
+            this.lstCommands.Location = new System.Drawing.Point(12, 261);
+            this.lstCommands.Name = "lstCommands";
+            this.lstCommands.Size = new System.Drawing.Size(86, 56);
+            this.lstCommands.TabIndex = 0;
+            this.lstCommands.Visible = false;
+            // 
+            // commandsLabel
+            // 
+            this.commandsLabel.AutoSize = true;
+            this.commandsLabel.Location = new System.Drawing.Point(104, 291);
+            this.commandsLabel.Name = "commandsLabel";
+            this.commandsLabel.Size = new System.Drawing.Size(129, 13);
+            this.commandsLabel.TabIndex = 2;
+            this.commandsLabel.Text = "Here are the commands : ";
+            this.commandsLabel.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(453, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "What do you want me to do for you, Sir? ";
+            this.label1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-4, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(236, 248);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(27, 24);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(189, 13);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(27, 24);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "voicEBot";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(87, 93);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(57, 55);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 242);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lstCommands);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.commandsLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(1100, 480);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "voicEBOT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ListBox lstCommands;
+        private System.Windows.Forms.Label commandsLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ ProjectJarvis/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Xml;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ProjectJarvis
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer JARVIS = new SpeechSynthesizer();
+        public NotifyIcon mynotify = new NotifyIcon();      
+        int count = 1;
+        int temp = 0;
+        
+        //Random rnd = new Random();
+
+        //[DllImport("user32")]
+        //public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        [DllImport("user32")]
+        public static extern void LockWorkStation();
+        public Form1()
+        {
+            
+            InitializeComponent();
+         
+            string[] commands = (File.ReadAllLines(@"C:\Users\imran\Desktop\New folder\command.txt"));
+            lstCommands.Items.Clear();
+            lstCommands.SelectionMode = SelectionMode.None;
+            lstCommands.Visible = false;
+            foreach (string command in commands)
+            {
+                lstCommands.Items.Add(command);
+            }
+
+            
+
+            string comport = "COM10";
+            serialPort1.PortName = comport;
+            serialPort1.BaudRate = 9600;
+            //if (!serialPort1.IsOpen)
+                //serialPort1.Open();
+            try
+            {
+                if (!serialPort1.IsOpen)
+                    serialPort1.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //for using system's default microphone . select audio input from devices ,files, or stream//
+            _recognizer.SetInputToDefaultAudioDevice();
+            
+            //Input Command file
+            _recognizer.LoadGrammar(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"C:\Users\imran\Desktop\New folder\command.txt")))));
+            _recognizer.LoadGrammar(new DictationGrammar());
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(_recognizer_SpeechRecognized);
+            //this specifies recognition does not terminate after completion
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            //JARVIS.Speak("goodbye jarvis");
+        }
+
+        void _recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //int ranNum = rnd.Next(1, 10);
+            string speech = e.Result.Text;
+            if (temp == 1)
+            {
+                speech = speech.Replace(" ", "+");
+                Process.Start("https://www.google.com.bd/?gws_rd=cr,ssl&ei=eoGUU4GvC9COuASioIGQAQ#q=" + speech);
+                temp = 0;
+                return;
+            }
+            switch (speech)
+            {
+                //GREETINGS
+                case "hi":
+                case "hey":
+                case "hello":
+                case "hello jarvis":
+                    //if (ranNum < 6)
+                    //{
+                    JARVIS.Speak("Hello sir");
+                    //}
+                    //else if (ranNum > 5)
+                    //{
+                    //JARVIS.Speak("Hi");
+                    //}
+                    break;
+                case "goodbye":
+                    JARVIS.Speak("See you later Sir!");
+                    Close();
+                    break;
+
+                //WEBSITES
+                case "open google":
+                    Process.Start("http://www.google.com");
+                    break;
+                case "open stackoverflow":
+                    Process.Start("http://www.stackoverflow.com");
+                    break;
+                case "open MSDN":
+                    Process.Start("https://msdn.microsoft.com/en-US/");
+                    break;
+                case "open yahoo":
+                    Process.Start("https://www.yahoo.com");
+                    break;
+                case "open tech shop":
+                    Process.Start("http://www.techshopbd.com/");
+                    break;
+                case "open speed test":
+                    Process.Start("http://www.speedtest.net/");
+                    break;
+                case "open wiki":
+                    Process.Start("http://www.wikipedia.org/");
+                    break;
+                case "open daily star":
+                    Process.Start("http://www.thedailystar.net/");
+                    break;
+                
+                //Search Google by speaking "find"
+                case "find":
+                    {
+                        JARVIS.Speak("What do you want to search sir !!");
+                        temp = 1;
+                    }
+                    break;
+                
+                //SHELL COMMANDS
+                case "run unity":
+                    Process.Start(@"C:\Program Files (x86)\Unity\Editor\Unity.exe");
+                    JARVIS.Speak("Loading Unity 3d engine!!");
+                    break;
+                case "run calculator":
+                    Process proc = new Process();
+                    proc.EnableRaisingEvents = false;
+                    proc.StartInfo.FileName = "calc";
+                    JARVIS.Speak("Loading Calculator");
+                    proc.Start();
+                    break;
+                case "run firefox":
+                    Process.Start(@"C:\Program Files (x86)\Mozilla Firefox\firefox.exe");
+                    JARVIS.Speak("Loading Mozilla Firefox");
+                    break;
+                case "run skype":
+                    Process.Start(@"C:\Program Files (x86)\Skype\Phone\Skype.exe");
+                    JARVIS.Speak("Loading Skype");
+                    break;
+                case "run IE":
+                    Process.Start(@"C:\Program Files\Internet Explorer\iexplore.exe");
+                    JARVIS.Speak("Loading Internet Explorer");
+                    break;
+                case "run command":
+                    Process proc1 = new Process();
+                    proc1.StartInfo.FileName = "cmd.exe";
+                    proc1.Start();
+                    break;
+
+                //CLOSE PROGRAMS
+                case "close unity":
+                    foreach (Process Proc1 in Process.GetProcesses())
+                    {
+                        if (Proc1.ProcessName.Equals("Unity"))
+                            Proc1.Kill();
+                    }
+                    break;
+                case "close calculator":
+                    foreach (Process Proc2 in Process.GetProcesses())
+                    {
+                        if (Proc2.ProcessName.Equals("calc"))
+                            Proc2.Kill();
+                    }
+                    break;
+                case "close firefox":
+                    foreach (Process Proc3 in Process.GetProcesses())
+                    {
+                        if (Proc3.ProcessName.Equals("firefox"))
+                            Proc3.Kill();
+                    }
+                    break;
+                case "close IE":
+                    foreach (Process Proc in Process.GetProcesses())
+                    {
+                        if (Proc.ProcessName.Equals("iexplore"))
+                            Proc.Kill();
+                    }
+                    break;
+
+                //CONDITION OF DAY
+                case "what time is it":
+                    DateTime now = DateTime.Now;
+                    string time = now.GetDateTimeFormats('t')[0];
+                    JARVIS.Speak(time);
+                    break;
+                case "what day is it":
+                    JARVIS.Speak(DateTime.Today.ToString("dddd"));
+                    break;
+                case "whats the date":
+                case "whats todays date":
+                    JARVIS.Speak(DateTime.Today.ToString("dd-MM-yyyy"));
+                    break;
+
+                case "switch window":
+                    SendKeys.Send("%{TAB " + count + "}");
+                    count += 1;
+                    break;
+
+                case "out of the way":
+                    if (WindowState == FormWindowState.Normal || WindowState == FormWindowState.Maximized)
+                    {
+                        WindowState = FormWindowState.Minimized;
+                        JARVIS.Speak("My apologies");
+                    }
+                    break;
+                case "come back":
+                    if (WindowState == FormWindowState.Minimized)
+                    {
+                        JARVIS.Speak("Alright Sir");
+                        WindowState = FormWindowState.Normal;
+                    }
+                    break;
+                case "show commands":
+                    string[] commands = (File.ReadAllLines(@"C:\Users\imran\Desktop\New folder\command.txt"));
+                    JARVIS.Speak("OK Sir");
+                    commandsLabel.Visible = true;
+                    lstCommands.Items.Clear();
+                    lstCommands.SelectionMode = SelectionMode.None;
+                    lstCommands.Visible = true;
+                    foreach (string command in commands)
+                    {
+                        lstCommands.Items.Add(command);
+                    }
+                    break;
+                case "hide listbox":
+                    lstCommands.Visible = false;
+                    commandsLabel.Visible = false;
+                    break;
+
+                //SHUTDOWN RESTART LOG OFF
+                case "shutdown":
+                    Process.Start("shutdown", "/s /t 0");
+                    break;
+                //case "log off":
+                //JARVIS.Speak("logging off");
+                //ExitWindowsEx(0, 0);
+                //break;
+                case "restart":
+                    Process.Start("shutdown", "/r /t 0");
+                    break;
+                case "lock PC":
+                    JARVIS.Speak("locking");
+                    LockWorkStation();
+                    break;
+
+                //Hardware Control
+                case "fan on":
+                    //arduino pin no.11 status : high
+                    serialPort1.Write("a");
+                    JARVIS.Speak("ok, sir");
+                    break;
+                case "fan off":
+                    //arduino pin no.11 status : low
+                    serialPort1.Write("A");
+                    JARVIS.Speak("fan is off, sir");
+                    break;
+
+                case "light on":
+                    //arduino pin no.12 status : high
+                    serialPort1.Write("b");
+                    JARVIS.Speak("ok, sir");
+                    break;
+                case "light off":
+                    //arduino pin no.12 status : low
+                    serialPort1.Write("B");
+                    JARVIS.Speak("light is off, sir");
+                    break;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            JARVIS.Speak("goodbye");
+            this.Close();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                mynotify.Visible = true;
+                this.ShowInTaskbar = false;
+                mynotify.Icon = SystemIcons.Application;
+                mynotify.BalloonTipText = "voicEBot running in the background, double click to restore";
+                mynotify.ShowBalloonTip(1000);
+
+            }
+
+            else if (this.WindowState == FormWindowState.Normal)
+            {
+                mynotify.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            mynotify.Visible = false;
+        }
+    }
+}
